aaacb8af6b65a943c1eadf58f2211c363f106710
Lia Couch <76275077+Liakini2@users.noreply.github.com>
2021-01-21 14:24:40 -0700

    app2

---
+++ app-2/src/Components/List.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+const List = () =>{
+    return (
+        <ul>
+            <h2>Cookies</h2>
+            <h2>Ice Cream</h2>
+            <h2>Gelato</h2>
+            <h2>Cake</h2>
+            <h2>Candy</h2>
+        </ul>
+    )
+}
+
+export default List
